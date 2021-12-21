e38fef2d961e748ec6ac295d2b814afca332ef5c
Madison Shimizu <mhshimizu@outlook.com>
2021-12-20 21:52:33 -1000

    completed

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,13 @@
 class Network < ActiveRecord::Base
+  has_many :shows
+  
+  def sorry
+    # passed_shows = Show.all.filter{|s| s.network_id != self.id}
+    "We're sorry about passing on John Mulaney's pilot"
+    # Show.all.filter{|s| s.network_id != self.id}.each {|p| puts "We're sorry about passing on #{p.name}'s pilot"}
+  end
   
 end
+
+# self.shows.filter{|s| s.network_id != self.id}
+# Show.create({name: "Full House", day: "Monday", season: "seven", genre: "Family", network_id: 4})
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        characters.map{ |char| char.actor.full_name}
+    end
 end
+
+
+# Actor.create ({first_name: "Jennifer", last_name: "Lawrence"})
+# Show.create ({name: "Hunger Games", day: "Friday", season: "one", genre: "Family")
+# Character.create ({name: "Katniss", actor_id: 1, show_id: 1, catchphrase: "I volunteer as tribute!"})
+
+# Actor.create ({first_name: "Emilia", last_name: "Clarke"})
+# Show.create ({name: "Game of Thrones", day: "Tuesday", season: "three", genre: "Fantasy", network_id: 1})
+# Character.create ({name: "Kahleesi", actor_id: 1, show_id: 2, catchphrase: "I am the Queen of Dragons"})
+# Network.create ({call_letters: "NBC", channel: 101})
